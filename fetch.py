2968ee926e6fa90e99c540c5e0700e0d4abbda0a
Md. Shafi Hussain <hussainshafi46@gmail.com>
2024-09-22 22:07:58 +0530

    .

--- fetch.py
+++ fetch.py
@@ -1,4 +1,5 @@
 from requests import get
+from pprint import pprint
 from json import dump
 from sys import argv
 
@@ -9,6 +10,7 @@ def fetchSongs(artist_id, atrist_name):
         songs_api = f'https://genius.com/api/artists/{artist_id}/songs?page={next_page}&per_page=50&sort=title'
         try:
             resp = get(songs_api).json()
+            pprint(resp)
             meta = resp['meta']
             #assert meta['status'] == 200
             next_page = resp['response']['next_page']
